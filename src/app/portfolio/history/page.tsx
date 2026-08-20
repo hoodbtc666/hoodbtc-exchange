2b95c00348e863d0db09c5ddf59ecffc7b8428e6
hoodbtc666 <peterhazim96@gmail.com>
2026-08-20 02:22:57 -0400

    Add Orderly portfolio history route

---
+++ src/app/portfolio/history/page.tsx
@@ -0,0 +1,4 @@
+"use client";
+import { HistoryModule } from "@orderly.network/portfolio";
+const { HistoryPage } = HistoryModule;
+export default function PortfolioHistoryPage(){ return <HistoryPage />; }
