7227e38ee7e6320c157b2b42a5760bbf80ab08cf
hoodbtc666 <peterhazim96@gmail.com>
2026-08-20 02:23:06 -0400

    Add Orderly portfolio settings route

---
+++ src/app/portfolio/setting/page.tsx
@@ -0,0 +1,4 @@
+"use client";
+import { SettingModule } from "@orderly.network/portfolio";
+const { SettingPage } = SettingModule;
+export default function PortfolioSettingPage(){ return <SettingPage />; }
